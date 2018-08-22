dccac0de5b83516afa63d41987d3b003066432b2
Pat <phorgan@gmail.com>
2018-08-22 21:59:32 +1000

    Added a visible clock, tweaked and tested single minute incrementing based off space key.

--- project/Main.gd
+++ project/Main.gd
@@ -6,28 +6,47 @@ var hour
 var minute
 
 func _ready():
-	# setup keypress?
+	# setup keypress
+	set_process_input(true)
 	# initialise HUD
 	day = 0
-	hour = 6
-	minute = 0
+	hour = 5
+	minute = 59
+	refreshHUD()
 	intro()
 	pass
 
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"): # Space or enter
+	pass
+
+func _input(event):
+	if Input.is_action_just_released("ui_accept"): # Space or enter
 		stopAlarm()
 	
 func intro():
 	# Play intro music
+	addMinute()
 	# Play alarm to interrupt
 	startAlarm()
 
+# TODO: This will need to be addMinutes(int minutesToAdd) eventually
+func addMinute():
+	minute = minute + 1
+	# Add leftover minutes with modulo etc
+	if (minute == 60):
+		hour = hour + 1
+		minute = 0
+	if (hour == 24):
+		day = day + 1
+		hour = 0
+
 func startAlarm():
+	$HUD/Label.add_color_override("font_color", Color(255,0,0,1))
 	pass
 
 func stopAlarm():
-	minute = minute + 1
+	#reset to green hex colour
+	$HUD/Label.add_color_override("font_color", Color("23eb07"))
 	refreshHUD()
 	
 func refreshHUD():
