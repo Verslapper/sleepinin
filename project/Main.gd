5cd9d14551fb618a922f7342169dc8b061296ef3
Pat <phorgan@gmail.com>
2018-09-01 22:10:49 +1000

    Game now functions as a cookie clicker style game until I put in levels (aka mini games) Music and sound effects are in place Next step is allowing the player to end and restart the game, with enough plot that there's a sense of scoring to quitting at different times

--- project/Main.gd
+++ project/Main.gd
@@ -4,6 +4,7 @@ extends Node
 var day
 var hour
 var minute
+var streamPlayer
 
 func _ready():
 	# setup keypress
@@ -12,22 +13,24 @@ func _ready():
 	day = 0
 	hour = 5
 	minute = 59
+	streamPlayer = AudioStreamPlayer.new()
+	self.add_child(streamPlayer)
+	streamPlayer.stream = load("res://assets/sleepin-bass.ogg")
+	streamPlayer.play()
 	refreshHUD()
-	intro()
-	pass
-
-func _process(delta):
-	pass
+	$Timer.start()
 
 func _input(event):
 	if Input.is_action_just_released("ui_accept"): # Space or enter
 		stopAlarm()
-	
-func intro():
-	# Play intro music
+
+func _on_Timer_timeout():
 	addMinute()
-	# Play alarm to interrupt
-	startAlarm()
+	$Timer.stop()
+	if (minute % 10 == 0):
+		startAlarm()
+	else:
+		$Timer.start()
 
 # TODO: This will need to be addMinutes(int minutesToAdd) eventually
 func addMinute():
@@ -39,20 +42,28 @@ func addMinute():
 	if (hour == 24):
 		day = day + 1
 		hour = 0
+	refreshHUD()
 
 func startAlarm():
-	$HUD/Label.add_color_override("font_color", Color("23eb07"))
+	$HUD/Label.add_color_override("font_color", Color(255,0,0,1))
+	streamPlayer.stream = load("res://assets/ding.ogg")
+	streamPlayer.play()
 	pass
 
 func stopAlarm():
 	#reset to green hex colour
-	$HUD/Label.add_color_override("font_color", Color(255,0,0,1))
+	$HUD/Label.add_color_override("font_color", Color("23eb07"))
 	refreshHUD()
+	streamPlayer.stop()
+	$Timer.start()
 	# This is how you switch levels
 	#get_tree().change_scene("Level1.tscn")
 	
 func refreshHUD():
-	$HUD/Label.set_text("Day " + str(day) + " " + paddedTo2Digits(hour) + ":" + paddedTo2Digits(minute))
+	var dayText = ""
+	if (day > 0):
+		dayText = "Day " + str(day) + " "
+	$HUD/Label.set_text(dayText + paddedTo2Digits(hour) + ":" + paddedTo2Digits(minute))
 	
 func paddedTo2Digits(time):
 	var padded
