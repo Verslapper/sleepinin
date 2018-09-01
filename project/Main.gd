af756fded9f598bc1ca2946695a69e724b9dcc49
Pat <phorgan@gmail.com>
2018-09-01 23:03:31 +1000

    Added label to display messages

--- project/Main.gd
+++ project/Main.gd
@@ -15,14 +15,18 @@ func _ready():
 	minute = 59
 	streamPlayer = AudioStreamPlayer.new()
 	self.add_child(streamPlayer)
-	streamPlayer.stream = load("res://assets/sleepin-bass.ogg")
-	streamPlayer.play()
+	#streamPlayer.stream = load("res://assets/sleepin-bass.ogg")
+	#streamPlayer.play()
 	refreshHUD()
 	$Timer.start()
 
 func _input(event):
 	if Input.is_action_just_released("ui_accept"): # Space or enter
 		stopAlarm()
+		$Timer.start()
+	if Input.is_action_just_released("ui_cancel"): # Escape
+		stopAlarm()
+		sleep()
 
 func _on_Timer_timeout():
 	addMinute()
@@ -32,10 +36,8 @@ func _on_Timer_timeout():
 	else:
 		$Timer.start()
 
-# TODO: This will need to be addMinutes(int minutesToAdd) eventually
 func addMinute():
 	minute = minute + 1
-	# Add leftover minutes with modulo etc
 	if (minute == 60):
 		hour = hour + 1
 		minute = 0
@@ -48,23 +50,42 @@ func startAlarm():
 	$HUD/Label.add_color_override("font_color", Color(255,0,0,1))
 	streamPlayer.stream = load("res://assets/ding.ogg")
 	streamPlayer.play()
-	pass
 
 func stopAlarm():
 	#reset to green hex colour
 	$HUD/Label.add_color_override("font_color", Color("23eb07"))
 	refreshHUD()
 	streamPlayer.stop()
-	$Timer.start()
 	# This is how you switch levels
 	#get_tree().change_scene("Level1.tscn")
+
+func sleep():
+	if (day == 0 && hour <= 6):
+		$HUD/PlotLabel.set_text("To: S.Bossman; Stuck in traffic, be there soon.")
+	elif (day == 0):
+		$HUD/PlotLabel.set_text("To: S.Bossman; Slept through my alarm, be there soon.")
+	else:
+		$HUD/PlotLabel.set_text("From: S.Bossman; Don't bother coming in tomorrow.")
 	
 func refreshHUD():
 	var dayText = ""
 	if (day > 0):
 		dayText = "Day " + str(day) + " "
 	$HUD/Label.set_text(dayText + paddedTo2Digits(hour) + ":" + paddedTo2Digits(minute))
-	
+	if (day == 0 && hour == 6 && minute == 20):
+		$HUD/PlotLabel.set_text("From: S.Bossman; Today's Agenda")
+	if (day == 0 && hour == 6 && minute == 29):
+		appendToPlot("From: L.Client; Help with setup")
+	if (day == 0 && hour == 9 && minute == 10):
+		appendToPlot("From: S.Bossman; Where are you?")
+	if (day == 0 && hour == 9 && minute == 45):
+		appendToPlot("From: C.Tractor; Can't start work without the demo")
+	if (day == 0 && hour == 11 && minute == 11):
+		appendToPlot("From: Y.Partner; Booked the place for tonight <3")
+
+func appendToPlot(text):
+	$HUD/PlotLabel.set_text($HUD/PlotLabel.text + text)
+
 func paddedTo2Digits(time):
 	var padded
 	if (time < 10):
